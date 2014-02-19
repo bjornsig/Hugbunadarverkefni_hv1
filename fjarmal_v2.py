c8d70b3a5b971dff00edfea1144ee01dd2781685
Tommi <Tommi@htgi-dw0928.rhi.hi.is>
2014-02-19 17:13:20 +0000

    kr-%-man

--- fjarmal_v2.py
+++ fjarmal_v2.py
@@ -218,10 +218,10 @@ class Lanasafn(tk.LabelFrame):
     def vidmot(self):
         merki = tk.Frame(self)
         tk.Label(merki, text='Verðtryggt').grid(row=0,column=0)
-        tk.Label(merki, text='Upphæð').grid(row=0,column=1)
+        tk.Label(merki, text='Upphæð [Kr.]').grid(row=0,column=1)
         tk.Label(merki, text='Heiti láns').grid(row=0,column=2)
-        tk.Label(merki, text='Vextir').grid(row=0,column=3)
-        tk.Label(merki, text='Tímabil').grid(row=0,column=4)
+        tk.Label(merki, text='Vextir [%]').grid(row=0,column=3)
+        tk.Label(merki, text='Tímabil [Mán]').grid(row=0,column=4)
         merki.columnconfigure(1,weight=2)
         merki.columnconfigure(2,weight=3)
         merki.columnconfigure(3,weight=1)
